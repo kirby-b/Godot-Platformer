14c2ac7e55563935e0470a95503daa7832a9935e
kirby-b <kirby.brodie66@gmail.com>
2024-02-11 10:39:42 -0500

    Day 2

--- Platformer-Tutorial/player.gd
+++ Platformer-Tutorial/player.gd
@@ -2,8 +2,7 @@ extends CharacterBody2D
 
 
 const SPEED = 100.0
-const JUMP_VELOCITY = -200.0
-
+const JUMP_VELOCITY = -220.0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
@@ -18,13 +17,15 @@ func _physics_process(delta):
 	# Handle jump.
 	if Input.is_action_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
+	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = move_toward(velocity.x,direction * SPEED, 10)
+	elif not direction and not is_on_floor():
+		velocity.x = move_toward(velocity.x,direction * SPEED, 10)
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, 20)
 
 	move_and_slide()
