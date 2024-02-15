3ae2c38b90bf9bc331bc4f9f4af1d28be59c228e
kirby-b <kirby.brodie66@gmail.com>
2024-02-15 09:55:00 -0500

    Day 3

--- Platformer-Tutorial/player.gd
+++ Platformer-Tutorial/player.gd
@@ -6,6 +6,8 @@ const JUMP_VELOCITY = -220.0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+@onready var sprite = $AnimatedSprite2D
+
 func _ready():
 	pass
 
@@ -13,7 +15,6 @@ func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
-
 	# Handle jump.
 	if Input.is_action_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
@@ -21,11 +22,21 @@ func _physics_process(delta):
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
+	if direction > 0:
+		sprite.flip_h = true
+	elif direction < 0:
+		sprite.flip_h = false
 	if direction:
 		velocity.x = move_toward(velocity.x,direction * SPEED, 10)
 	elif not direction and not is_on_floor():
 		velocity.x = move_toward(velocity.x,direction * SPEED, 10)
 	else:
 		velocity.x = move_toward(velocity.x, 0, 20)
+	if is_on_floor() and direction:
+		sprite.play("run")
+	elif is_on_floor() and not direction:
+		sprite.play("idle")
+	elif not is_on_floor():
+		sprite.play("jump")
 
 	move_and_slide()
