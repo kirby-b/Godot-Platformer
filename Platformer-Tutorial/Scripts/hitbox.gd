cfd811c4843992790895f16b03e6047cb70cfd00
Brodie E Kirby <118732723+kirby-b@users.noreply.github.com>
2024-03-18 11:20:42 -0400

    Update hitbox.gd

--- Platformer-Tutorial/Scripts/hitbox.gd
+++ Platformer-Tutorial/Scripts/hitbox.gd
@@ -8,14 +8,15 @@ var iframes = false
 
 func _on_hitbox_body_entered(body):
 	if iframes == false:
-		# Hurts the player
+		# Hurts the player and activates i frames
 		if body is Player and instant_kill == false:
 			body.player_hurt()
 			invincibility(body)
 		elif body is Player and instant_kill == true:
-			body.player_death()
+			body.player_death() #Kills the player
 
 func invincibility(body):
+	#Waits for a time so the player doesnt lose all health immediately
 	timer.start()
 	iframes = true
 	await timer.timeout
