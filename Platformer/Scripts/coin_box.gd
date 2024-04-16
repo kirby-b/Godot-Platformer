3a9bf45e02b4e7b5f638a9fedbbaf18eb8cb03bf
Brodie E Kirby <118732723+kirby-b@users.noreply.github.com>
2024-04-16 10:18:32 -0400

    I think ray cast should work

--- Platformer/Scripts/coin_box.gd
+++ Platformer/Scripts/coin_box.gd
@@ -1,6 +1,7 @@
 extends StaticBody2D
 
 @onready var sprite = $AnimatedSprite2D
+@onready var hit_detect = $GetHitBoi
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
