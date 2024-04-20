a2c7a2e17b24e6228d54c6f315ab4a06984760e0
kirby-b <kirby.brodie66@gmail.com>
2024-04-20 00:22:18 -0400

    Now allows multiple coins in a box

--- Platformer/Scripts/coin_box.gd
+++ Platformer/Scripts/coin_box.gd
@@ -10,13 +10,15 @@ enum{
 }
 
 # Exports 
-@export() var coins_in_box: int
+@export() var coins_in_box: int = 1
 
 # On ready variables to access nodes
 @onready var sprite = $AnimatedSprite2D
 @onready var hit_detect = $GetHitBoi
+@onready var collect_buffer = $CollectBuffer
 
 var state = ACTIVE # Holds the current state
+var collected = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -33,8 +35,12 @@ func _process(delta):
 
 func active():
 	sprite.play("full")
-	if is_player():
+	if is_player() and collected < coins_in_box:
 		Events.emit_signal("coin_get")
+		collected += 1
+		collect_buffer.start()
+		await collect_buffer.timeout
+	elif collected == coins_in_box:
 		state = DEACTIVE
 		# Basically I want to check if the collider is player, then I need to check if they are in 
 		# the air. If all that passes, it gives the player a coin.
