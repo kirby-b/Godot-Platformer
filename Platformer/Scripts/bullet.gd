2e8695c339a16b02e7f754cf7a947972e5de73ec
kirby-b <kirby.brodie66@gmail.com>
2024-05-02 15:55:33 -0400

    new function to aim bullet
    - Gets a direction for where it faces and fires

--- Platformer/Scripts/bullet.gd
+++ Platformer/Scripts/bullet.gd
@@ -3,9 +3,15 @@ extends Area2D
 var speed = 750
 
 func _physics_process(delta):
-	position += transform.x * speed * delta
+	position += transform.x * speed * delta 
 
-func _on_Bullet_body_entered(body):
+
+func _on_body_entered(body):
 	if body.is_in_group("enemies"):
 		body.queue_free()
 	queue_free()
+	
+func aim(direction):
+	$Sprite2D.flip_h = direction > 0
+	speed *= direction
+	
