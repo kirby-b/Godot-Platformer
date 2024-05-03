519a30cc5d7750b25bec7adff7bc63898605a45d
kirby-b <kirby.brodie66@gmail.com>
2024-05-03 15:41:07 -0400

    Bullet now cannot leave the screen

--- Platformer/Scripts/bullet.gd
+++ Platformer/Scripts/bullet.gd
@@ -16,3 +16,6 @@ func aim(direction):
 	$Sprite2D.flip_h = direction > 0
 	speed *= direction
 	
+# Removes the bullet when it leaves the screen
+func _on_visible_on_screen_notifier_2d_screen_exited():
+	queue_free()
