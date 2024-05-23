5f05379ff55d7f2a312b53778c91c5b23d795ef4
Brodie E Kirby <118732723+kirby-b@users.noreply.github.com>
2024-05-23 09:08:05 -0400

    Boss script

---
+++ Platformer/Scripts/boss1.gd
@@ -0,0 +1,5 @@
+extends CharacterBody2D
+
+# The boss should follow a sort of pattern where they pop out of a pipe
+# and then fire a bullet in left or right. They will have a relatively high
+# amount of health and should be fairly easy for a first boss.
