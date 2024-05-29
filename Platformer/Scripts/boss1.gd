e4d5248583c8d0a12b8ddb919731dfcf83526840
kirby-b <kirby.brodie66@gmail.com>
2024-05-28 21:37:03 -0400

    Just messing with the boss stuff
    Not very developed yet, but I have some basics

--- Platformer/Scripts/boss1.gd
+++ Platformer/Scripts/boss1.gd
@@ -1,5 +1,22 @@
 extends CharacterBody2D
 
-# The boss should follow a sort of pattern where they pop out of a pipe
-# and then fire a bullet in left or right. They will have a relatively high
-# amount of health and should be fairly easy for a first boss.
+@onready var lazer_delay = $LazerDelay
+@onready var fire_point = $LazerSpawn
+var can_fire = true
+
+# Gets the lazer scene for instancing
+@export var Lazer : PackedScene
+var aim_direction = 1
+
+# Makes the gun shoot
+func shoot():
+	var l = Lazer.instantiate()
+	l.aim(aim_direction)
+	get_tree().current_scene.add_child(l)
+	l.transform = fire_point.global_transform
+	SoundPlayer.play_sound(SoundPlayer.BANG)
+	can_fire = false
+	lazer_delay.start()
+
+func _on_lazer_delay_timeout():
+	can_fire = true
