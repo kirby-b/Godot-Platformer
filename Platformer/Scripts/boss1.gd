a41c3362af6ff2f750666aae82422097d1729e9f
kirby-b <kirby.brodie66@gmail.com>
2024-05-29 22:36:24 -0400

    Update boss1.gd
    Made the boss have life and only walk after the battle starts

--- Platformer/Scripts/boss1.gd
+++ Platformer/Scripts/boss1.gd
@@ -3,7 +3,6 @@ extends CharacterBody2D
 @onready var lazer_delay = $LazerDelay
 @onready var fire_point = $LazerSpawn
 @onready var sprite = $AnimatedSprite2D
-var can_fire = true
 
 # Gets the lazer scene for instancing
 @export var Lazer : PackedScene
@@ -12,6 +11,8 @@ var can_fire = true
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var aim_direction = 1
 var direction = 1
+var can_fire = true
+var life = 10
 
 func _physics_process(delta):
 	if GlobalVars.activeboss == true:
@@ -19,21 +20,21 @@ func _physics_process(delta):
 			shoot()
 			aim_direction *= -1
 		#Set boss position to random tube point
-	# Adds the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
-	# Detects if they hit a wall
-	var found_wall = is_on_wall() 
-	
-	# Changes direction if they find a wall of ledge
-	if found_wall:
-		direction *= -1
-	# Gets the flip with a simple boolean
-	sprite.flip_h = direction > 0
-	
-	sprite.play("walk") # Constantly plays the walk
-	velocity.x = direction * speed # Constant speed
-	move_and_slide()
+		# Adds the gravity.
+		if not is_on_floor():
+			velocity.y += gravity * delta
+		# Detects if they hit a wall
+		var found_wall = is_on_wall() 
+		
+		# Changes direction if they find a wall of ledge
+		if found_wall:
+			direction *= -1
+		# Gets the flip with a simple boolean
+		sprite.flip_h = direction > 0
+		
+		sprite.play("walk") # Constantly plays the walk
+		velocity.x = direction * speed # Constant speed
+		move_and_slide()
 
 # Makes the gun shoot
 func shoot():
@@ -48,3 +49,10 @@ func shoot():
 
 func _on_lazer_delay_timeout():
 	can_fire = true
+	
+# Makes the boss lose a life
+func lose_life():
+	life -= 1
+	if life <= 0:
+		SoundPlayer.play_sound(SoundPlayer.HURT)
+		queue_free()
