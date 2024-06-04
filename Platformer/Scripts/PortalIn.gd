f96d2f9e100f74f65c0abb89cd3f1a445458298d
kirby-b <kirby.brodie66@gmail.com>
2024-06-03 20:11:43 -0400

    Prepped the portal in and out scripts

--- Platformer/Scripts/PortalIn.gd
+++ Platformer/Scripts/PortalIn.gd
@@ -1,1 +1,20 @@
-extends Marker2D
+extends Sprite2D
+
+@onready var exit = $"../PortalCenter"
+
+func _ready():
+	pass
+
+func _physics_process(_delta):
+	Events.connect("boss_entered", Callable(self, "_on_boss_enter"))
+	Events.connect("boss_defeated", Callable(self, "_on_boss_defeat"))
+
+func _on_area_2d_body_entered(body):
+	if body is Player or body is Boss:
+		body.position = exit.get_global_transform().origin
+
+func _on_boss_enter():
+	pass
+
+func _on_boss_defeat():
+	pass
--- Platformer/Scripts/PortalOut.gd
+++ Platformer/Scripts/PortalOut.gd
@@ -1,1 +1,14 @@
-extends Marker2D
+extends Sprite2D
+
+func _ready():
+	pass
+
+func _physics_process(_delta):
+	Events.connect("boss_entered", Callable(self, "_on_boss_enter"))
+	Events.connect("boss_defeated", Callable(self, "_on_boss_defeat"))
+
+func _on_boss_enter():
+	pass
+
+func _on_boss_defeat():
+	pass
